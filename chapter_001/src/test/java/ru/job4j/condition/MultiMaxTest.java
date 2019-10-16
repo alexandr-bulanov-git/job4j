61d4f6a66ae8ab054090e5e459d48d983cf44f74
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-10-16 19:34:28 +0300

    Максимум из трех чисел [#190]

---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(5, 2, 3);
+        assertThat(result, is(5));
+    }
+}
