a906692b9324b367b6e3d44a38c2cd59d35fef9c
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-10-25 19:15:20 +0300

    Отладка программы в IDEA (Максимум 4х чисел) [#159464]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -4,15 +4,12 @@ public class SqMax {
     public static int max(int first, int second, int third, int forth) {
         int result = forth;
         if (first > second & first > third & first > forth) {
-           result = first;
-        }
-     else if (second > first & second > third & second > forth) {
-           result = second;
-        }
-     else if (third > first & third > second & third > forth) {
+            result = first;
+        } else if (second > first & second > third & second > forth) {
+            result = second;
+        } else if (third > first & third > second & third > forth) {
             result = third;
-        }
-     else  if (first == second & second == third & third == forth) {
+        } else  if (first == second & second == third & third == forth) {
             result = first;
         }
         return result;
