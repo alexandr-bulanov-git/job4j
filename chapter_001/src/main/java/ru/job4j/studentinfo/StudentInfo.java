2abfd350ea6522b1d6e7a01052a97ae405ed5aa8
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-10-25 19:45:28 +0300

    3. Коммиты на решения задач.[#18069]

--- chapter_001/src/main/java/ru/job4j/studentinfo/StudentInfo.java
+++ chapter_001/src/main/java/ru/job4j/studentinfo/StudentInfo.java
@@ -1,9 +1,9 @@
-package ru.job4j.studentInfo;
+package ru.job4j.studentinfo;
 
 import java.util.Arrays;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Alex Bulanov");
         System.out.println("25.09.2019");
     }
