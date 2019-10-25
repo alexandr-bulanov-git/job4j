dca438fa43ef460ad09868fbfee2bd6c2af02b7b
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-10-25 19:23:18 +0300

    3. Коммиты на решения задач.[#18069]

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,13 +1,21 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static int rubleToEuro(int value) { return value / 70; }
+    public static int rubleToEuro(int value)  {
+        return value / 70;
+    }
 
-    public static int rubleToDollar(int value) { return value / 60; }
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
 
-    public static int dollarToRuble(int value) { return value * 60; }
+    public static int dollarToRuble(int value) {
+        return value * 60;
+    }
 
-    public static int euroToRuble(int value) { return value * 70; }
+    public static int euroToRuble(int value) {
+        return value * 70;
+    }
 
 
     public static void main(String[] args) {
