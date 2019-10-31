c763b8b029721ef5df387e1932a60e0c832fa719
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-10-31 20:01:18 +0300

    Вызов метод с возвращаемым типом. [#167387]

--- chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -1,4 +1,11 @@
 package ru.job4j.oop;
+/**
+ * Вызов метод с возвращаемым типом.
+ *
+ * @author Alexandr Bulanov
+ * @version $Id$
+ * @since 31.10.2019
+ */
 
 public class DummyDic {
     public static void main(String[] args) {
