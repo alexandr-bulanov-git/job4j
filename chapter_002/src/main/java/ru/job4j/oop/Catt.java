e9b58d5c968ea1bb985e7a4952dd82b0d9bdfd7e
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-11-01 19:18:55 +0300

    Поля объекта [#166083]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Catt.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Catt {
+    private String food;
+    private String name = "Romka";
+
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + "eat" + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Catt gav = new Catt();
+        Catt nick = new Catt();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Catt black = new Catt();
+        black.eat("fish");
+        black.show();
+    }
+}
