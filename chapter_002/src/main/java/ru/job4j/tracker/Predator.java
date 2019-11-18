93b6f8dd7a778ed2ad75996a0d0fe89fab9ea70f
Alexandr Bulanov <alexandr.bulanov.git@gmail.com>
2019-11-18 19:40:01 +0300

    Перегрузка конструктора. [#173890]

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class Predator extends Animal {
     public Predator() {
+        super();
         System.out.println("Predator");
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class Tiger extends Animal {
    public Tiger() {
-        System.out.println("Tiger");
+       super();
+       System.out.println("Tiger");
     }
 }
